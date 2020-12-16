d582ab751745afefe0fd4791fb9fcc9cec6f7a4d
Tyler Tracy <me@tylertracy.com>
2020-12-15 18:24:27 -0600

    Day 15 (part 2 takes a lot of time)

---
+++ 2020/day15/part1.cpp
@@ -0,0 +1,49 @@
+#include <fstream>
+#include <iostream>
+#include <vector>
+#include <map>
+using namespace std;
+
+vector<int> nums;
+
+void readData() {
+  fstream file;
+  file.open("data.txt", ios::in);
+
+  int num;
+  char comma;
+  while (file >> num) {
+    nums.push_back(num); 
+    file >> comma;
+  }
+}
+
+int playGame() {
+  map<int, int> lastSaid;
+  int lastNum = 0;
+
+  for (int i = 0; i < 2020; i++) {
+    int nextNum;
+    if (i < nums.size()) {
+      nextNum = nums[i]; 
+    } else if (lastSaid.count(lastNum) == 0) {
+      nextNum = 0; 
+    } else {
+      nextNum = i - (lastSaid[lastNum]+1);
+    }
+
+    if (i > 0) {
+      lastSaid[lastNum] = i -1;
+    }
+    lastNum = nextNum;
+  }
+
+  return lastNum;
+}
+
+
+int main() {
+  readData();
+  int res = playGame();
+  cout << "Result: " << res << endl;
+}
